d5d329e730c4e8f6c6267a285bce572aade98a77
salskhal <olaniyisal@gmail.com>
2022-03-24 05:33:39 +0100

    Features/ Fetched movies from API

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,59 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/" class="logo">SalsMovie</router-link>
+    <div class="movie-search">
+      <input type="text" />
+      <button>Search</button>
+    </div>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
-#app {
+
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap');
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  min-height: 100vh;
+  background: orange;
+  font-family: "Inter", sans-serif;
+}
+
+nav{
+  display: flex;
+  padding: 10px 20px;
+  justify-content: space-between;
+}
+
+.logo{
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
+  text-transform: uppercase;
+  font-size: 2rem;
+  transition: all .25s ease-in-out;
+}
+
+.logo:hover{
+  color: rgb(230, 221, 221);
+}
+
+
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
+} */
 
-nav {
+/* nav {
   padding: 30px;
 }
 
@@ -26,5 +64,5 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,62 @@
 <template>
   <div class="home">
-   <h1>Hello World</h1>
+    <h1 class="title">MOVIES</h1>
+    <div class="movie-card">
+      <div v-for="movie in movies" :key="movie.id" class="movies">
+        <img :src="imagepath + movie.backdrop_path" :alt="movie.poster_path" class="image">
+        <h4>{{ movie.original_title }}</h4>
+        <p>{{}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      movies: [],
+      apikey: "7dd2f6061023c47cd5775913d9e60359",
+      imagepath: "https://image.tmdb.org/t/p/original"
+    };
+  },
+  mounted() {
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${this.apikey}&language=en-US`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        this.movies = data.results;
+      });
+  },
+};
 </script>
+
+
+
+<style>
+.title {
+  text-align: center;
+}
+
+.movie-card{
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  padding-inline: 50px;
+}
+
+.movies{
+  cursor: pointer;
+  transition: all .2s ease-in-out
+}
+
+.movies:hover{
+  transform: scale(1.05);
+}
+
+
+.image{
+  width: 100%
+}
+</style>
